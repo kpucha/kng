7eef9d62b68f36dd516f43e97f6fb180fdd64f7b
Arturo Alcalá <alcala28043@gmail.com>
2023-03-03 18:21:24 +0100

    pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ServiceWorkerModule } from '@angular/service-worker';
 
@@ -42,6 +42,12 @@ export function HttpLoaderFactory(httpBackend: HttpBackend) {
     FontAwesomeModule,
     NgxEchartsModule.forRoot({
       echarts: () => import('echarts'),
+    }),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
     }),
   ],
   providers: [],
